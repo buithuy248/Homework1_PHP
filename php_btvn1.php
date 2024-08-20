81fbee4c9c70d7f4d2474485a688c077570e5687
buithuy248 <boctru24082004@gmail.com>
2024-08-20 21:58:50 +0700

    add php_btvn1

---
+++ php_btvn1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework1</title>
+</head>
+<body style="background-color: Cornsilk; color: SaddleBrown; text-align: justify; margin: 50px 100px; font-size: 1.3em;">
+    <h1 style="text-align: center; color: Chocolate; font-size: 2.5em;">HÔM NAY LÀ THỨ MẤY?</h1>
+    <?php
+        echo '<table style="margin: 0 auto; border: 2px solid SaddleBrown; border-collapse: separate; border-spacing: 10px; background-color: NavajoWhite; width: auto;">';
+        echo '<td style="padding: 20px; font-size: 1.5em; border: 1px solid SaddleBrown; color: SaddleBrown;">Thứ 5, ngày 22 tháng 8 năm 2024</td>';
+        echo '<td style="padding: 20px; font-size: 1.5em; border: 1px solid SaddleBrown; color: SaddleBrown;">Thursday, August 22<sup>nd</sup>, 2024</td>';
+        echo '</table>';
+
+        echo '<h2 style="font-size: 2em;">I. Thông tin chung</h2>';
+        echo '<p><b>- Âm lịch:</b> 19-7-2024 <i>(Ngày giáp tý, tháng nhâm thân, năm giáp thìn)</i></p>';
+        echo '<p>- Là ngày thứ 234 trong năm 2024</p>';
+        echo '<p>- Thuộc tuần thứ 35 của năm 2024</p>';
+
+        echo '<h2 style="font-size: 2em;">II. Sự kiện, ngày lễ</h2>';
+        echo '<p><i>Xem thêm tại: </i>';
+        echo '<a href="https://www.capapham.com/ngay-22-8-august-22-ngay-nay-nam-xua-on-this-day/" style="color: Chocolate; font-size: 1.2em; text-decoration: none;">August 22: On this day</a>';
+        echo '</p>';
+
+        echo '<div style="text-align: right; margin-top: 10px;">';
+        echo '<button style="background-color: NavajoWhite; border: 1px solid SaddleBrown; padding: 10px 20px; font-size:1.2em; cursor: pointer;">';
+        echo '<p style="color:SaddleBrown;">Ngày mai là thứ mấy?</p>';
+        echo '</button>';
+        echo '</div>';
+    ?>
+</body>
+</html>
